b4b78bb5bffd5d8e0ba8537b5e02f3c04b04e75e
Quoc Lam <LamQuoc.24@gmail.com>
2022-10-17 15:36:39 -0400

    Time: 101 ms (53.57%), Space: 42.6 MB (56.07%) - LeetHub

--- 1304-find-n-unique-integers-sum-up-to-zero/1304-find-n-unique-integers-sum-up-to-zero.js
+++ 1304-find-n-unique-integers-sum-up-to-zero/1304-find-n-unique-integers-sum-up-to-zero.js
@@ -4,36 +4,50 @@
  */
 var sumZero = function(n) {
     
-    let answer = []
-    let m = n
+//     let answer = []
+//     let m = n
     
-    if(n===1){
-        return ["0"]
-    } 
-    else if( (n % 2) === 0){
+//     if(n===1){
+//         return ["0"]
+//     } 
+//     else if( (n % 2) === 0){
         
-        while (answer.length < m){
-            answer.push(-n);
-            answer.push(n)
-            n--
-        }
-    } else {
+//         while (answer.length < m){
+//             answer.push(-n);
+//             answer.push(n)
+//             n--
+//         }
+//     } else {
         
-        while (answer.length < m-1){
+//         while (answer.length < m-1){
             
-            answer.push(-n);
-            answer.push(n)
-            n--
+//             answer.push(-n);
+//             answer.push(n)
+//             n--
             
-        }
+//         }
         
-    }
+//     }
     
-    if( (m % 2) !== 0){
-        answer.push(0)
-    }
-    console.log(answer)
+//     if( (m % 2) !== 0){
+//         answer.push(0)
+//     }
+//     console.log(answer)
             
-    return answer
+//     return answer
     
+    
+  var num = Math.floor(n/2); 
+  var res = [];
+
+  for(var i=1;i<=num;i++){
+      res.push(i,-i)
+     } 
+
+  if(n%2!==0){
+    res.push(0)
+  }
+  
+  return res 
+
 };
