c1f47a2765c992abb186072db4194aaae037c595
Quoc Lam <LamQuoc.24@gmail.com>
2022-10-18 15:38:02 -0400

    Time: 140 ms (17.80%), Space: 44.5 MB (17.80%) - LeetHub

--- 0258-add-digits/0258-add-digits.js
+++ 0258-add-digits/0258-add-digits.js
@@ -4,18 +4,24 @@
  */
 var addDigits = function(num) {
     
-    while (num.toString().length >1){
+//     while (num.toString().length >1){
         
-        let numbr = num.toString();
-        let newNum = 0;
-        for (let i=0; i<numbr.length;i++){
+//         let numbr = num.toString();
+//         let newNum = 0;
+//         for (let i=0; i<numbr.length;i++){
             
-            newNum += parseInt(numbr[i])
-            console.log(newNum)
+//             newNum += parseInt(numbr[i])
+//             console.log(newNum)
             
-        }
-        num = newNum
+//         }
+//         num = newNum
         
-    }
-    return num
+//     }
+//     return num
+    
+        num = num.toString();
+    while (num.length !== 1) {
+        num = num.split('').reduce((a,i)=>+a + +i).toString();
+    }  
+    return num;
 };
