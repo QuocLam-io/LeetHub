b9bc96657a5e3a4d0404c412eec0dac05dee93a5
Quoc Lam <LamQuoc.24@gmail.com>
2022-10-28 13:29:53 -0400

    Time: 127 ms (80.58%), Space: 47.5 MB (60.55%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -2,41 +2,26 @@
  * @param {string} s
  * @return {number}
  */
-var lengthOfLongestSubstring = function(str) {
+var lengthOfLongestSubstring = function(s) {
 
-//     let maxLen = 0,
-//     maxStr = '',
-//     tmpStr,
-//     posIndex,
-//     i;
+    let maxLen = 0,
+    maxStr = '',
+    tmpStr,
+    posIndex,
+    i;
 
-//   for( i = 0 ; i < s.length; i++ ){
+  for( i = 0 ; i < s.length; i++ ){
 
-//     tmpStr = s[i];
-//     posIndex = maxStr.indexOf(tmpStr);
+    tmpStr = s[i];
+    posIndex = maxStr.indexOf(tmpStr);
 
-//     if(posIndex > -1){
-//       maxStr = maxStr.substring(posIndex + 1);
-//     }
-
-//     maxStr += tmpStr;
-//     maxLen = Math.max(maxLen, maxStr.length);
-//   }
-
-//   return maxLen;
-      let longest = 0;
-  let seen = {};
-  let start = 0;
-
-  for (let i = 0; i < str.length; i++) {
-    let char = str[i];
-    if (seen[char]) {
-      start = Math.max(start, seen[char]);
+    if(posIndex > -1){
+      maxStr = maxStr.substring(posIndex + 1);
     }
-    // index - beginning of substring + 1 (to include current in count)
-    longest = Math.max(longest, i - start + 1);
-    // store the index of the next char so as to not double count
-    seen[char] = i + 1;
+
+    maxStr += tmpStr;
+    maxLen = Math.max(maxLen, maxStr.length);
   }
-  return longest;
+
+  return maxLen;
 };
