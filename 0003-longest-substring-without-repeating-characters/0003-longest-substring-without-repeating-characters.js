a12d9388920be768eaa0f36a493946a997419fe4
Quoc Lam <LamQuoc.24@gmail.com>
2022-10-28 13:32:46 -0400

    Time: 147 ms (63.82%), Space: 48.2 MB (49.46%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -2,26 +2,22 @@
  * @param {string} s
  * @return {number}
  */
-var lengthOfLongestSubstring = function(s) {
+var lengthOfLongestSubstring = function(str) {
 
-    let maxLen = 0,
-    maxStr = '',
-    tmpStr,
-    posIndex,
-    i;
+    
+      let longest = 0;
+  let seen = {};
+  let start = 0;
 
-  for( i = 0 ; i < s.length; i++ ){
-
-    tmpStr = s[i];
-    posIndex = maxStr.indexOf(tmpStr);
-
-    if(posIndex > -1){
-      maxStr = maxStr.substring(posIndex + 1);
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+    if (seen[char]) {
+      start = Math.max(start, seen[char]);
     }
-
-    maxStr += tmpStr;
-    maxLen = Math.max(maxLen, maxStr.length);
+    // index - beginning of substring + 1 (to include current in count)
+    longest = Math.max(longest, i - start + 1);
+    // store the index of the next char so as to not double count
+    seen[char] = i + 1;
   }
-
-  return maxLen;
+  return longest;
 };
